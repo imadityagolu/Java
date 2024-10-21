e0b343d296c044585d23ca81465610cb5a5ab1fe
Aditya Kumar <42202518+imadityagolu@users.noreply.github.com>
2024-10-21 12:35:31 +0530

    Create fibonachi-series.java

---
+++ questions/fibonachi-series.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+  public static void main(String[] args) {
+
+int n = 3;
+      int i = 0;
+      int a = 0;
+      int b = 1;
+      int c;
+      while(i<n){
+          c = a +b;
+          a = b;
+          b = c;
+          i++;
+      }
+      System.out.print(a);
+  }
+}
